709fe2d6d79ce07e45c602ec269789a6f79c2be2
SunbossRS <yaorizhuangzhu@163.com>
2020-02-04 21:58:29 +0800

    Rel update

---
+++ js/playrun.js
@@ -0,0 +1,38 @@
+// Playrun javascript
+// This is a auto run on website's javascript file
+
+// Website mobile device sidebar show & hide
+function side() {
+	if(!$('#sidctrl').hasClass('sidebar-open')){
+		$('#sidctrl').addClass('sidebar-open');
+	} else {
+		$('#sidctrl').removeClass('sidebar-open');
+	}
+};
+
+// Run a probe object website mobile device sidebar new feature
+window.onload = function()
+	{
+		if ($('aside .sidebar').blur) {
+			$('#sidctrl').removeClass('sidebar-open');
+		}
+	}
+
+// Website search framework
+SimpleJekyllSearch({
+	searchInput: document.getElementById('searchbox'),
+    resultsContainer: document.getElementById('resultbox'),
+    json: '{{ site.baseurl }}/search.json',
+    searchResultTemplate: '<li><a href="{url}" class="sidebar-heading nav-item nav-link">{title}</a></li>'
+})
+
+// Run a probe object resultbox show & hide on website
+window.onload = function()
+	{
+		if ($("#searchbox").focus) {
+			$('#resultbox').show();
+		};
+		if ($("#searchbox").blur) {
+				$('#resultbox').hide();
+		};
+	}
